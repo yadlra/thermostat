ca4522e9fc44c708de2094056fbab51b451bc8d5
yadlra <yadi.sb1@gmail.com>
2019-05-01 15:53:02 +0100

    set minimum temperature and throws error when minimum temperature exceeded

--- jasmine/jasmine-standalone-3.4.0/spec/thermostatSpec.js
+++ jasmine/jasmine-standalone-3.4.0/spec/thermostatSpec.js
@@ -4,7 +4,7 @@ describe('Thermostat', function() {
  describe('initialization', function() {
   it('starts at 20 degrees', function (){
    thermostat = new Thermostat ();
-   expect(thermostat.temperature).toEqual(20);
+   expect(thermostat.getTemperature()).toEqual(20);
   });
  });
 
@@ -13,26 +13,35 @@ describe('Thermostat', function() {
      it('increase the temperature by one degree', function() {
        thermostat = new Thermostat ();
        thermostat.tempUp();
-       expect(thermostat.temperature).toEqual(21);
+       expect(thermostat.getTemperature()).toEqual(21);
      });
      it('increase the temperature by two degrees', function() {
        thermostat = new Thermostat ();
        thermostat.tempUp();
        thermostat.tempUp();
-       expect(thermostat.temperature).toEqual(22);
+       expect(thermostat.getTemperature()).toEqual(22);
      });
    });
    describe('decrease temperature', function() {
      it('decrease the temperature by one degree', function() {
        thermostat = new Thermostat ();
        thermostat.tempDown();
-       expect(thermostat.temperature).toEqual(19);
+       expect(thermostat.getTemperature()).toEqual(19);
      });
      it('decrease the temperature by two degrees', function() {
        thermostat = new Thermostat ();
        thermostat.tempDown();
        thermostat.tempDown();
-       expect(thermostat.temperature).toEqual(18);
+       expect(thermostat.getTemperature()).toEqual(18);
+     });
+     it('stops decreasing when minimum temperature is reached', function() {
+       thermostat = new Thermostat();
+       for (var i = 20; i > 10; i--) {
+         thermostat.tempDown();
+       }
+       expect(() => {
+         thermostat.tempDown()}
+       ).toThrowError(TypeError,'Minimum temperature reached!');
      });
    });
  });
--- jasmine/jasmine-standalone-3.4.0/src/thermostat.js
+++ jasmine/jasmine-standalone-3.4.0/src/thermostat.js
@@ -1,5 +1,10 @@
 var Thermostat = function() {
   this.temperature = 20;
+  this.MINIMUN_TEMPERATURE = 10;
+};
+
+Thermostat.prototype.getTemperature = function () {
+  return this.temperature;
 };
 
 Thermostat.prototype.tempUp = function () {
@@ -7,5 +12,8 @@ Thermostat.prototype.tempUp = function () {
 };
 
 Thermostat.prototype.tempDown = function () {
+  if (this.temperature === this.MINIMUN_TEMPERATURE) {
+    throw new TypeError('Minimum temperature reached!');
+  };
   this.temperature -= 1;
 };
