fffe0ab6e28263ed41e365095a9239a67baa3da5
Jake Napper <napperjlg@gmail.com>
2019-05-01 14:48:58 +0100

    can increase temperature by one degree at a time

--- jasmine/jasmine-standalone-3.4.0/spec/thermostatSpec.js
+++ jasmine/jasmine-standalone-3.4.0/spec/thermostatSpec.js
@@ -7,4 +7,20 @@ describe('Thermostat', function() {
    expect(thermostat.temperature).toEqual(20);
   });
  });
+
+ describe('change temperature', function() {
+   describe('increase temperature', function() {
+     it('increase the temperature by one degree', function() {
+       thermostat = new Thermostat ();
+       thermostat.tempUp();
+       expect(thermostat.temperature).toEqual(21);
+     });
+     it('increase the temperature by two degrees', function() {
+       thermostat = new Thermostat ();
+       thermostat.tempUp();
+       thermostat.tempUp();
+       expect(thermostat.temperature).toEqual(22);
+     });
+   });
+ });
 });
--- jasmine/jasmine-standalone-3.4.0/src/thermostat.js
+++ jasmine/jasmine-standalone-3.4.0/src/thermostat.js
@@ -1,3 +1,7 @@
 var Thermostat = function() {
   this.temperature = 20;
 };
+
+Thermostat.prototype.tempUp = function () {
+  this.temperature += 1;
+};
