8790cbcab7a8569ffb551f81fc7c75c268e62536
yadlra <yadi.sb1@gmail.com>
2019-05-01 16:58:42 +0100

    refactored test

--- jasmine/jasmine-standalone-3.4.0/spec/thermostatSpec.js
+++ jasmine/jasmine-standalone-3.4.0/spec/thermostatSpec.js
@@ -1,9 +1,12 @@
 
 describe('Thermostat', function() {
   var thermostat;
+  beforeEach(function(){
+    thermostat = new Thermostat();
+  });
+
  describe('initialization', function() {
   it('starts at 20 degrees', function (){
-   thermostat = new Thermostat ();
    expect(thermostat.getTemperature()).toEqual(20);
   });
  });
@@ -11,12 +14,10 @@ describe('Thermostat', function() {
  describe('change temperature', function() {
    describe('increase temperature', function() {
      it('increase the temperature by one degree', function() {
-       thermostat = new Thermostat ();
        thermostat.tempUp();
        expect(thermostat.getTemperature()).toEqual(21);
      });
      it('increase the temperature by two degrees', function() {
-       thermostat = new Thermostat ();
        thermostat.tempUp();
        thermostat.tempUp();
        expect(thermostat.getTemperature()).toEqual(22);
@@ -24,18 +25,15 @@ describe('Thermostat', function() {
    });
    describe('decrease temperature', function() {
      it('decrease the temperature by one degree', function() {
-       thermostat = new Thermostat ();
        thermostat.tempDown();
        expect(thermostat.getTemperature()).toEqual(19);
      });
      it('decrease the temperature by two degrees', function() {
-       thermostat = new Thermostat ();
        thermostat.tempDown();
        thermostat.tempDown();
        expect(thermostat.getTemperature()).toEqual(18);
      });
      it('stops decreasing when minimum temperature is reached', function() {
-       thermostat = new Thermostat();
        for (var i = 20; i > 10; i--) {
          thermostat.tempDown();
        }
@@ -45,6 +43,7 @@ describe('Thermostat', function() {
        });
      });
    });
+
    describe('power saving mode', function() {
      describe('is on or off', function() {
        it('is on by default', function() {
